d76a2a03993ba59a27a73a8f24df2c89d044c0f9
astudillo <astudillo@astudillo-Satellite-L845.(none)>
2014-04-30 11:46:32 -0500

    no esta muy lindo pero lo hace

--- compras/admin.py
+++ compras/admin.py
@@ -4,13 +4,32 @@ from django.contrib import admin
 
 from compras.models import Producto, Pedido, Lineapedido
 
+class LineapedidoInline(admin.StackedInline):
+    model = Lineapedido
+
+
 class PedidoAdmin(admin.ModelAdmin):
     list_display = ['usuario','fecha']
+    inlines = [LineapedidoInline]
 
-class LineapedidoAdmin(admin.ModelAdmin):
-    list_display = ['productop','pedidoid','cantidad']
 
 
 admin.site.register(Producto)
 admin.site.register(Pedido,PedidoAdmin)
-admin.site.register(Lineapedido,LineapedidoAdmin)
+
+
+
+
+
+# class RepuestoInline(admin.StackedInline):
+#     model = Repuesto
+#     extra = 3
+
+# class ProductoAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         ('Producto', {'fields': ['articulo', 'marca', 'referencia']}),
+#         ('Inventario', {'fields': ['cantidad']}),
+#         ('Dimensiones y Peso', {'fields': ['largo', 'ancho', 'alto', 'peso']}),
+#         ('Precios', {'fields': ['preciocompra', 'precioventa', 'preciopromocion']}),
+#     ]
+#     inlines = [RepuestoInline]
